a84ade32e24cf0eae51ee164706d6cfa0de2f469
Milorad Orzes <miloradorzes@milorads-mac.lan>
2017-03-11 07:59:59 +0100

    Initial Commit

---
+++ SelectableButtonCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelectableButtonCell
+//
+//  Created by Milorad Orzes on 11/03/2017.
+//  Copyright © 2017 Milorad Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelectableButtonCell/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SelectableButtonCell
+//
+//  Created by Milorad Orzes on 11/03/2017.
+//  Copyright © 2017 Milorad Orzes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
